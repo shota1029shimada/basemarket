d1283e313a838803e1010d02ffdebc58dcbff9d6
shota shimada <a71drst8@gmail.com>
2025-12-11 17:18:24 +0900

    2025-12-11:first commit: BaseMarket project

---
+++ src/main/java/com/basemarket/BaseMarketApplication.java
@@ -0,0 +1,13 @@
+package com.basemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basemarket/BaseMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
